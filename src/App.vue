4754cc72059ff9b0924f1a96ab04c2220d0879a5
Mona Lisa <gugacns94@gmail.com>
2019-07-05 18:15:54 +0200

    zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import AppPosts from "./components/AppPosts";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import AppPosts from './components/AppPosts'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: `/`, redirect: `/posts` },
+  { path: `/posts`, component: AppPosts },
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
